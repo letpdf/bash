96fcd2d5320e68212eb590b3e5fff92a2916fce0
tom-sapletta-com <tom@sapletta.com>
2022-07-07 18:13:23 +0200

    update

--- README.md
+++ README.md
@@ -43,3 +43,25 @@ To extract the data from the set of pdf files, the invoive2textdata.py file is e
         config.json         : Has path to Source and Destination.
 
  
+
+
+    issuer: Amazon Web Services, Inc.
+    keywords:
+    - Amazon Web Services
+    fields:
+      amount: TOTAL AMOUNT DUE ON.*\$(\d+\.\d+)
+      amount_untaxed: TOTAL AMOUNT DUE ON.*\$(\d+\.\d+)
+      date: Invoice Date:\s+([a-zA-Z]+ \d+ , \d+)
+      invoice_number: Invoice Number:\s+(\d+)
+      partner_name: (Amazon Web Services, Inc\.)
+    options:
+      remove_whitespace: false
+      currency: HKD
+      date_formats:
+        - '%d/%m/%Y'
+    lines:
+        start: Detail
+        end: \* May include estimated US sales tax
+        first_line: ^    (?P<description>\w+.*)\$(?P<price_unit>\d+\.\d+)
+        line: (.*)\$(\d+\.\d+)
+        last_line: VAT \*\*
--- pdf2txt.py
+++ pdf2txt.py
@@ -3,8 +3,23 @@ from invoice2data import extract_data
 from invoice2data.extract.loader import read_templates
 import json
 import os
+import zoneinfo
+from datetime import datetime
+import pytz
+import warnings
+import dateparser
+
+# Ignore dateparser warnings regarding pytz
+warnings.filterwarnings(
+    "ignore",
+    message="The localize method is no longer necessary, as this time zone supports the fold attribute",
+)
+
+NYC = zoneinfo.ZoneInfo("America/New_York")
+datetime(2020, 1, 1, tzinfo=NYC)
 
 ALLOWED_EXTENSIONS = set(['pdf'])
+DATE_FORMAT = "%d.%m.%Y"
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -25,52 +40,54 @@ try:
         print("EXIST")
     else:
         print("NOT EXIST")
+        exit()
 
     if allowed_file(pathname):
-        print(pathname)
+        #print(pathname)
         #print(type(pathname))
         #print(type(templates))
         #print(read_template)
         #path_filename.append(pathname)
         #result = extract_data(pathname)
+
         result = extract_data(pathname, templates)
-        print(result)
+        #stz = get_localzone()
+        #date_obj = date_obj.replace(tzinfo=stz)
 
-        if (not result):
+        #if (not result):
+        if (result==False):
             print('This PDF is currently not supported please make sure that you are using a orignal statment.')
+            exit()
 
-        result['date']=result['date'].strftime("%d.%m.%Y")
+        result['date']=result['date'].strftime(DATE_FORMAT)
 
         if "sale_date" in result:
-            result['sale_date']=result['sale_date'].strftime("%d.%m.%Y")
+            result['sale_date']=result['sale_date'].strftime(DATE_FORMAT)
 
         if "subscription_date" in result:
-            result['subscription_date']=result['subscription_date'].strftime("%d.%m.%Y")
+            result['subscription_date']=result['subscription_date'].strftime(DATE_FORMAT)
+                #.replace(tzinfo=NYC)
+
 
         print(result)
-        print("----------------------")
-        if (result==False):
-            #pdfFiles.append(filename)
-            exit()
-        else:
-            #pdfFiles.append(result)
-            with open(pathname + ".json", 'w') as file:
-                file.write(json.dumps(result))
+        #pdfFiles.append(result)
+        with open(pathname + ".json", 'w') as file:
+            file.write(json.dumps(result))
 
-            with open(pathname + ".date", 'w') as file:
-                file.write(str(result['date']))
+        with open(pathname + ".date", 'w') as file:
+            file.write(str(result['date']))
 
-            with open(pathname + ".price", 'w') as file:
-                file.write(str(result['amount']))
+        with open(pathname + ".price", 'w') as file:
+            file.write(str(result['amount']))
 
-            with open(pathname + ".number", 'w') as file:
-                file.write(str(result['invoice_number']))
+        with open(pathname + ".number", 'w') as file:
+            file.write(str(result['invoice_number']))
 
-            with open(pathname + ".currency", 'w') as file:
-                currency=str(result['currency'])
-                currency=currency.replace("€", "EUR" )
-                currency=currency.replace("$", "USD" )
-                file.write(currency)
+        with open(pathname + ".currency", 'w') as file:
+            currency=str(result['currency'])
+            currency=currency.replace("€", "EUR" )
+            currency=currency.replace("$", "USD" )
+            file.write(currency)
 
 except FileNotFoundError:
     print(f'{argv[1]} not found')
